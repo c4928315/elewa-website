a2861ae8f83ec2844741060fc61abd54b7b8e2f2
Chrispine Ochieng <chrispineochieng@Chrispines-MacBook-Pro.local>
2023-08-16 10:34:35 +0300

    generated a partners section component file

---
+++ libs/pages/home/components/README.md
@@ -0,0 +1,7 @@
+# pages-home-components
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test pages-home-components` to execute the unit tests.
---
+++ libs/pages/home/components/src/lib/pages-home-components.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PartnersSectionComponent } from './partners-section/partners-section.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [PartnersSectionComponent, PartnersSectionComponent],
+})
+export class PagesHomeComponentsModule {}
