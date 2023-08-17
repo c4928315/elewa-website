753d81c5f5347e6d739a9fb532d08020fdac2da3
Chrispine Ochieng <chrispineochieng@Chrispines-MacBook-Pro.local>
2023-08-17 11:49:44 +0300

    added styles responsible for responsiveness

--- libs/pages/home/components/src/lib/partners-section/partners-section.component.ts
+++ libs/pages/home/components/src/lib/partners-section/partners-section.component.ts
@@ -5,4 +5,35 @@ import { Component } from '@angular/core';
   templateUrl: './partners-section.component.html',
   styleUrls: ['./partners-section.component.scss'],
 })
-export class PartnersSectionComponent {}
+export class PartnersSectionComponent {
+  images = [
+    {
+      id: 1,
+      url: "https://theme-intraart.myshopify.com/cdn/shop/files/brand-logo-01.png?v=1613766027"
+    },
+    {
+      id: 2,
+      url: "https://theme-intraart.myshopify.com/cdn/shop/files/brand-logo-01.png?v=1613766027"
+    },
+    {
+      id: 3,
+      url: "https://theme-intraart.myshopify.com/cdn/shop/files/brand-logo-03.png?v=1613766027"
+    },
+    {
+      id: 4,
+      url: "https://theme-intraart.myshopify.com/cdn/shop/files/brand-logo-04.png?v=1613766027"
+    },
+    {
+      id: 5,
+      url: "https://theme-intraart.myshopify.com/cdn/shop/files/brand-logo-05.png?v=1613766027"
+    },
+    {
+      id: 6,
+      url: "https://theme-intraart.myshopify.com/cdn/shop/files/brand-logo-06.png?v=1613766027"
+    },
+    {
+      id: 7,
+      url: "https://theme-intraart.myshopify.com/cdn/shop/files/brand-logo-07.png?v=1613766027"
+    }
+  ]
+}
